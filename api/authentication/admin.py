917f8bcb679f57763dcbb8e351cd9d64cf52e74f
Thalida Noel <thalida.c.noel@gmail.com>
2023-04-04 02:02:26 +0000

    Revert "Reset"
    This reverts commit f368d3c7774819076b50874e07d4e0695294b6aa.

--- api/authentication/admin.py
+++ api/authentication/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin, GroupAdmin as BaseGroupAdmin
+from social_django.admin import UserSocialAuthOption as BaseUserSocialAuthAdmin
+from social_django.models import UserSocialAuth
 from unfold.admin import ModelAdmin
 from unfold.forms import AdminPasswordChangeForm, UserChangeForm, UserCreationForm
 
@@ -19,4 +21,12 @@ admin.site.unregister(Group)
 
 @admin.register(Group)
 class GroupAdmin(BaseGroupAdmin, ModelAdmin):
+    pass
+
+
+admin.site.unregister(UserSocialAuth)
+
+
+@admin.register(UserSocialAuth)
+class UserSocialAuthAdmin(BaseUserSocialAuthAdmin, ModelAdmin):
     pass
